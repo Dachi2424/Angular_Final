197f66254a463505a4f19be751302fc04d0f1abb
Dachi Darjania <dachidar19@gmail.com>
2025-11-22 02:02:44 +0400

    davamate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { CartDetails } from './cart-details/cart-details';
+import { Details } from './details/details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: Home},
+  {path: "/cart-details", component: CartDetails},
+  {path: "/details", component: Details},
+];
---
+++ src/app/services/api.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Api {
+  constructor(private api: HttpClient) {
+
+  }
+
+  //products
+  productsAll(){
+    return this.api.get(`https://api.everrest.educata.dev/shop/products/all?page_index=1&page_size=16`)
+  }
+
+  productsSearch() {
+    return this.api.get(`https://api.everrest.educata.dev/shop/products/search?page_index=1&page_size=16&brand=asus&rating=3&price_min=100&price_max=600`)
+  }
+}
