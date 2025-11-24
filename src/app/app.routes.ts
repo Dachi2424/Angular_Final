699a56a45bdc2d7b555ad05b87d411309cefea0d
Dachi Darjania <dachidar19@gmail.com>
2025-11-24 17:56:16 +0400

    signup design over

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { CartDetails } from './cart-details/cart-details';
 import { Details } from './details/details';
+import { Signin } from './signin/signin';
+import { Signup } from './signup/signup';
 
 export const routes: Routes = [
   {path: "", component: Home},
   {path: "cart-details", component: CartDetails},
   {path: "details", component: Details},
+  {path: "signin", component: Signin},
+  {path: "signup", component: Signup},
 ];
